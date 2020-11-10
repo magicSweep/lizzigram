a0ed42ade6d39f9af69806e56b0cbf2077f48e9c
magicLost <oooze@mail.ru>
2020-11-10 19:27:39 +0300

    Ready to start

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  //testEnvironment: "node",
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>/node_modules/identity-obj-proxy",
+    "^.+\\.module\\.(css|sass|scss)$":
+      "<rootDir>/node_modules/identity-obj-proxy",
+  },
+
+  roots: ["<rootDir>/src", "<rootDir>/server"],
+
+  testMatch: ["<rootDir>/src/**/*.test.js", "<rootDir>/server/**/*.test.js"],
+
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    //"^.+\\.[t|j]sx?$": "ts-jest",
+    //"^.+\\.tsx?$": "<rootDir>/node_modules/ts-jest",
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)":
+      "<rootDir>/config/jest/fileTransform.js",
+  },
+
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+class Hello {
+  say = () => {
+    console.log("Hello");
+  };
+}
+
+const hello = new Hello();
+hello.say();
+
+export default Hello;
